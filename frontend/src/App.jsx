723937dc858cf3dbdd1b9c77ed3af1411f21506e
sungbbang <sinsa5133@gmail.com>
2025-03-27 14:25:17 +0900

    라우팅 설정 및 페이지 컴포넌트 생성

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,54 @@
 import React from 'react';
 import NavBar from './components/NavBar/NavBar';
-import { BrowserRouter } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
 import Footer from './components/Footer/Footer';
+import HomePage from './pages/HomePage';
+import FixturesPage from './pages/FixturesPage';
+import RecordPage from './pages/RecordPage';
+import BoardPage from './pages/BoardPage';
+import ContactPage from './pages/ContactPage';
+
+function Layout() {
+  return (
+    <>
+      <NavBar />
+      <Outlet />
+      <Footer />
+    </>
+  );
+}
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: '/fixtures',
+        element: <FixturesPage />,
+      },
+      {
+        path: '/record',
+        element: <RecordPage />,
+      },
+      {
+        path: '/board',
+        element: <BoardPage />,
+      },
+      {
+        path: '/contact',
+        element: <ContactPage />,
+      },
+    ],
+  },
+]);
 
 function App() {
-  return (
-    <BrowserRouter>
-      <NavBar />
-      <Footer />
-    </BrowserRouter>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
